85a0e1b82e9beabb36d0f2a8109b1c4b7d16c174
王小朋 <xiaopengmonsters@gmail.com>
2018-05-05 18:05:47 +0800

    Initial Commit

---
+++ DataStructure_Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructure_Algorithm
+//
+//  Created by xiaopeng on 2018/5/5.
+//  Copyright © 2018年 student. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
